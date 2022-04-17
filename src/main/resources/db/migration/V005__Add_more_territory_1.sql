ee63d05a16a06c01293cf2985b58846146fefcc8
Ross Taylor-Turner <ross.turner001@googlemail.com>
2022-04-17 21:11:10 +0100

    Fix for logic on updates, and added requested territory selections

--- src/main/resources/db/migration/V005__Add_more_territory_1.sql
+++ src/main/resources/db/migration/V005__Add_more_territory_1.sql
@@ -1,5 +1,5 @@
 INSERT INTO territory_selection (territory_id, display_name, title_list, admin_only)
 VALUES
     (28, 'Östergötland & Kinda', 'c_ostergotland,c_kinda', false),
-    (29, 'Skara & Värmaland', 'c_skara,c_varmland', false)
+    (29, 'Skara & Värmland', 'c_skara,c_varmland', false)
 ;
